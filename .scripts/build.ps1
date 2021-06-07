fb75c474a57d8267491f96c30b7fe76dbebded04
Iridar51 <mail@iridar.net>
2021-06-07 08:58:28 +0300

    Add CHL submodule.

--- .scripts/build.ps1
+++ .scripts/build.ps1
@@ -12,6 +12,8 @@ Write-Host "Sourcing $common"
 
 $builder = [BuildProject]::new("X2WOTCCommunityPromotionScreen", $srcDirectory, $sdkPath, $gamePath)
 
+$builder.IncludeSrc("$srcDirectory\X2WOTCCommunityHighlander\X2WOTCCommunityHighlander\Src")
+
 switch ($config)
 {
     "debug" {
